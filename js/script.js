70c3b5a98eb1e9d0312e6c0afe37ed7c41980a32
Donavan Armstrong <donavan.mc@gmail.com>
2019-05-07 17:59:10 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// global variables//
+const item = document.getElementsByClassName('student-item');
+const itemsPerPage = 10;
+const page = 1;
+
+//this functions hides all the students excepts the 10 students you will see per page//
+function showPage(list, page) {
+   const startIndex = (page * itemsPerPage) - 1;
+   const endIndex = startIndex - 9;
+   
+   for (let i = 0; i < list.length; i++) {
+      if (i >= endIndex && i <= startIndex) {
+         list[i].style.display = 'block';
+      } else {
+         list[i].style.display = 'none';
+      }
+   }
+}
+
+//this functions appends the dom elements for the pagination links//
+function appendPageLinks(list) {
+   const pages = Math.ceil(list.length/10);
+   const div = document.createElement('div');
+   const ul = document.createElement('ul');
+   const pageDiv = document.querySelector('.page');
+   div.classList.add("pagination");
+   pageDiv.appendChild(div);
+   div.appendChild(ul);
+
+   
+
+   for (let i = 0; i < pages; i++) {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.className = 'active';
+      a.href = '#';
+      a.textContent = i;
+      ul.appendChild(li);
+      li.appendChild(a);
+      a.addEventListener('click', (e) => {
+         showPage(item, i);
+         e.target.className = "active";
+      })
+   }
+
+}
+
+showPage(item, page);
+appendPageLinks(item);
