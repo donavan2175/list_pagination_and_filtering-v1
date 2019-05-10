025ab1ad5a9a9e5db75aea3c87b7c31373bb1aae
Donavan Armstrong <donavan.mc@gmail.com>
2019-05-09 17:08:41 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const page = 1;
 
 //this functions hides all the students excepts the 10 students you will see per page//
 function showPage(list, page) {
-   const startIndex = (page * itemsPerPage) - 1;
+   const startIndex = page * itemsPerPage - 1;
    const endIndex = startIndex - 9;
    
    for (let i = 0; i < list.length; i++) {
@@ -27,18 +27,21 @@ function appendPageLinks(list) {
    pageDiv.appendChild(div);
    div.appendChild(ul);
 
-   
 
-   for (let i = 0; i < pages; i++) {
+
+   for (let i = 1; i <= pages; i++) {
       const li = document.createElement('li');
       const a = document.createElement('a');
-      a.className = 'active';
       a.href = '#';
       a.textContent = i;
       ul.appendChild(li);
       li.appendChild(a);
       a.addEventListener('click', (e) => {
          showPage(item, i);
+         const activePage = document.getElementsByClassName('active');
+         if (activePage.length > 0) {
+            activePage[0].className = activePage[0].className.replace('active', '');
+         }
          e.target.className = "active";
       })
    }
